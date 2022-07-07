de506a75d5a23ac0acf37a0a3962b6c57c876a94
Sagar Jangid <60397881+sagarjangid41@users.noreply.github.com>
2022-07-07 16:35:27 +0530

    my 1st commit

--- mycode.java
+++ mycode.java
@@ -3,6 +3,6 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("hello i am dev1"); 
-        System.out.println("sagar change i am jshdj,asbdkas"); 
+        System.out.println("hello this is sagar "); 
     }
 }
